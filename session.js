2aae8fd8d593f32cc9f73afcbcbc4089a2de7214
SCAR-LK <dgimshan79@gmail.com>
2025-09-02 19:59:38 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~UAwWajA#ZrNeG7ImuBjDoMRKgc7-T2h2wKe7nGVBQVCTEiMjEsQ",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~ZUAwWajA#ZrNeG7ImuBjDoMRKgc7-T2h2wKe7nGVBQVCTEiMjEsQ",
 PORT: process.env.PORT || "8000"
 };
