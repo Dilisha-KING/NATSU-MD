85a3e27cff68125e8012f0a609bd42ec9ea2bd76
SCAR-LK <dgimshan79@gmail.com>
2025-09-02 21:32:23 +0530

    Update session.js

--- session.js
+++ session.js
@@ -1,18 +1,11 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-// auto-create session directory
-const SESSION_DIR = process.env.SESSION_DIR || './session';
-if (!fs.existsSync(SESSION_DIR)) {
-    fs.mkdirSync(SESSION_DIR, { recursive: true });
-}
-
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+return text === fault ? true : false;
 }
-
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~ZUAwWajA#ZrNeG7ImuBjDoMRKgc7-T2h2wKe7nGVBQVCTEiMjEsQ",
-  PORT: process.env.PORT || "8000",
-  SESSION_DIR
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~ZUAwWajA#ZrNeG7ImuBjDoMRKgc7-T2h2wKe7nGVBQVCTEiMjEsQ",
+PORT: process.env.PORT || "8000"
 };
+
